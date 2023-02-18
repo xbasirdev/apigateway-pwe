44f56cb285afc69a7f36b23e63d7889939c6bdc7
Diana Moran <dcmoran169@gmail.com>
2023-02-18 16:35:20 -0400

    correccion de permisos y de bug en app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -44,8 +44,7 @@ $app->alias('mail.manager', Illuminate\Contracts\Mail\Factory::class);
 $app->alias('mailer', Illuminate\Mail\Mailer::class);
 $app->alias('mailer', Illuminate\Contracts\Mail\Mailer::class);
 $app->alias('mailer', Illuminate\Contracts\Mail\MailQueue::class);
-class_alias(Tymon\JWTAuth\Facades\JWTAuth::class, 'JWTAuth');
-class_alias(Tymon\JWTAuth\Facades\JWTFactory::class, 'JWTFactory');
+
 
 /*
 |--------------------------------------------------------------------------
